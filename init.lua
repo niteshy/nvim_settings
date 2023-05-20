3002e0baf2306e9b4ae71141fa39d4046941a8cd
Nitesh Yadav <nitesh@cloudera.com>
2023-05-20 13:34:31 -0700

    Working copy till lsp

---
+++ init.lua
@@ -0,0 +1,13 @@
+
+require "myconfig.plugins-setup"
+require "myconfig.core.options"
+require "myconfig.core.keymaps"
+require "myconfig.core.colorscheme"
+require "myconfig.plugins.comment"
+require "myconfig.plugins.nvim-tree"
+require "myconfig.plugins.telescope"
+require "myconfig.plugins.nvim-cmp"
+require "myconfig.plugins.lsp.mason"
+require "myconfig.plugins.lsp.lspsaga"
+require "myconfig.plugins.lsp.lspconfig"
+
