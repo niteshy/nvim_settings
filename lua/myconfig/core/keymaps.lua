d756db4c8d107152621d6e83b7d2927fa46e62c4
Nitesh Yadav <nitesh@cloudera.com>
2023-05-21 00:16:56 -0700

    Working final config

--- lua/myconfig/core/keymaps.lua
+++ lua/myconfig/core/keymaps.lua
@@ -37,7 +37,7 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
 -- nvim-tree
-keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
 
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>") -- find files within current working directory, respects .gitignore
